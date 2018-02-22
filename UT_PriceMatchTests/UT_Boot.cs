37eda1f044ef75a717a835a63a78790ac5b841b5
Павел Храпкин <Khrapkin.P@csitltd.ru>
2018-02-22 16:12:54 +0300

    UT_Boot cleanup

--- UT_PriceMatchTests/UT_Boot.cs
+++ UT_PriceMatchTests/UT_Boot.cs
@@ -11,7 +11,7 @@
  * UT_getHpath - тест protected getHpath() - читает Hpath из Propetries
  */
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PriceMatch;
+using System.Collections.Generic;
 
 namespace PriceMatch.Tests
 {
@@ -31,72 +31,22 @@ namespace PriceMatch.Tests
             //    Assert.AreEqual(boot.Suppliers.AllSuppliers.Count, boot.ssInit.Count,
             //        "Не все Поставщики отображаются в SupplierInit.xml");
         }
-    }
 
-    [TestClass()]
-    public class UT_Bootstrap
+        [TestMethod()]
+        public void UT_getHpath()
+        {
+            _Boot boot = new _Boot();
+            Dictionary<string, string> v = boot._getHpath();
+
+            Assert.IsTrue(boot.VERSION.Contains("PriceMatch v"));
+            Assert.IsTrue(v["#DEBUG"].Contains("Desktop\\$DebugDir"));
+            Assert.IsTrue(v["#TOC"].Contains("\\common\\exceldesign"));
+            Assert.AreEqual(v["#TOC"], boot.TOCdir);
+        }
+    } // end UT_Boot class
+
+    internal class _Boot : Boot
     {
-        //[TestMethod()]
-        //public void UT_Boot()
-        //{
-        //    Boot boot = new Boot();
-        //    Assert.IsTrue(boot.VERSION.Contains("Price"));
-        //    Assert.IsTrue(boot.fo.IsDirExist(boot.TOCdir));
-        //    Assert.IsTrue(boot.fo.IsDirExist(boot.DebugDir));
-        //    Assert.IsTrue(boot.fo.IsDirExist(boot.ExcelDir));
-        //    Assert.IsTrue(boot.Suppliers.AllSuppliers.Count > 0);
-        //    Assert.IsTrue(boot.ssInit.Count > 0);
-        //    Assert.AreEqual(boot.Suppliers.AllSuppliers.Count, boot.ssInit.Count,
-        //        "Не все Поставщики отображаются в SupplierInit.xml");
-        //}
-    }
-}
-
-////using Microsoft.VisualStudio.TestTools.UnitTesting;
-////using PriceMatch;
-////using System;
-////using System.Collections.Generic;
-////using System.Linq;
-////using System.Text;
-////using System.Threading.Tasks;
-
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Collections.Generic;
-
-//namespace PriceMatch.Tests
-//{
-//    [TestClass()]
-//    public class UT_Bootstrap
-//    {
-//        [TestMethod()]
-//        public void UT_Boot()
-//        {
-//            Boot boot = new Boot();
-//            Assert.IsTrue(boot.VERSION.Contains("Price"));
-//            Assert.IsTrue(boot.fo.IsDirExist(boot.TOCdir));
-//            Assert.IsTrue(boot.fo.IsDirExist(boot.DebugDir));
-//            Assert.IsTrue(boot.fo.IsDirExist(boot.ExcelDir));
-//            Assert.IsTrue(boot.Suppliers.AllSuppliers.Count > 0);
-//            Assert.IsTrue(boot.ssInit.Count > 0);
-//            Assert.AreEqual(boot.Suppliers.AllSuppliers.Count, boot.ssInit.Count,
-//                "Не все Поставщики отображаются в SupplierInit.xml");
-//        }
-
-//        [TestMethod()]
-//        public void UT_getHpath()
-//        {
-//            _Boot boot = new _Boot();
-//            Dictionary<string, string> v = boot._getHpath();
-
-//            Assert.IsTrue(boot.VERSION.Contains("PriceXLS v"));
-//            Assert.IsTrue(v["#DEBUG"].Contains("Desktop\\$DebugDir"));
-//            Assert.IsTrue(v["#TOC"].Contains("\\common\\exceldesign"));
-//            Assert.AreEqual(v["#TOC"], boot.TOCdir);
-//        }
-//    } // end UT_Boot class
-
-//    internal class _Boot : Boot
-//    {
-//        internal Dictionary<string, string> _getHpath() => getHpath();
-//    } // end _Boot class
-//} // end namespace
+        internal Dictionary<string, string> _getHpath() => getHpath();
+    } // end _Boot class
+} // end namespace
